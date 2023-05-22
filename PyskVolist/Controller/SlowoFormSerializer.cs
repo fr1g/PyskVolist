61e79ef6345b714880b163df7ea3e2b3e48f4360
Feiron <53085628+fr1g@users.noreply.github.com>
2023-05-22 09:39:34 +0800

    Serializer done
    WIP:
     - Deserializer
     - FrontEnd Design

--- PyskVolist/Controller/SlowoFormSerializer.cs
+++ PyskVolist/Controller/SlowoFormSerializer.cs
@@ -1,15 +1,56 @@
 ﻿using System;
-using PyskVolist.Model.Slowo
+using PyskVolist.Model;
+
 namespace PyskVolist.Controller
 {
 	public class SlowoFormSerializer
 	{
 		// 反序列化: 从文本转换为类?
 		// 序列化: 类转换为文本?
-		public string? Serlize(Slowo S)
+		private string? _data, _accWord;
+		private int? _hi;
+        // 纯文本格式: \{[dir]/[udir],}&{[sv]/[nsv].}word;\::type #Translation(patez)
+        // 优化纯文本格式: ,完成体+未完成体: (或者) .定向%不定向: (或者) 词语本身 = 类型#汉译(接格关系)
+        // 类型: 名词, 形容词, 数词, 代词, 动词, 副词, 前置, 连接, 语气, 感叹, 形动
+        // 具体化类型: (无: 默认的默认, 不作细化特殊标记) 自动: 录入时候自动识别
+        /* 名词: 阳性, 阴性, 中性, (默认)自动, 无
+		 * 数词: 基数, 序数, (默认)自动, 无
+		 * 动词: 第一变位, 第二变位, 特殊变位, 无
+		 */
+        // 重音所在的字母序号(№ / ^ ) 或者 重音标 (*)
+		// @ ,xx:.yy:zz*a=tt#cc(pp)
+        public string? Serlize(Slowo S)
+		{
+			_data = "@";
+			if (!S.ForceBasic)
+			{
+                if (S.Direction is not null) _data += $",{S.Direction}:";
+                if (S.SV is not null) _data += $".{S.SV}:";
+				if (S.HardIndex is not null) _accWord = S.Word + $"*{++S.HardIndex}";
+				else _accWord = S.Word;
+            }
+
+			_data += $"{(_accWord is null? S.Word : _accWord)}=";
+
+			if (!S.ForceBasic)
+				if (S.WordType is not null) _data += $"{S.WordType}#";
+            
+            _data += $"{S.Translation}";
+
+            if (!S.ForceBasic)
+                if (S.Follow is not null) _data += $"({S.Follow})";            
+
+			return _data;
+		}
+
+		public Slowo? Deselize(string cons)
 		{
 
+
+
+			return null;
 		}
+
 		public SlowoFormSerializer()
 		{
 		}
--- PyskVolist/Model/Slowo.cs
+++ PyskVolist/Model/Slowo.cs
@@ -6,22 +6,24 @@ namespace PyskVolist.Model
 	{
         // 什么时候能够查询bkrs获取单词///////
         // 纯文本格式: \{[dir]/[udir],}&{[sv]/[nsv].}word;\::type #Translation(patez)
-        // 优化纯文本格式: 完成体+未完成体: (或者) 定向%不定向: (或者) 词语本身 : 类型=汉译(接格关系)
+        // 优化纯文本格式: ,完成体+未完成体: (或者) .定向%不定向: (或者) 词语本身 = 类型#汉译(接格关系)
+
         // 类型: 名词, 形容词, 数词, 代词, 动词, 副词, 前置, 连接, 语气, 感叹, 形动
         // 具体化类型: (无: 默认的默认, 不作细化特殊标记) 自动: 录入时候自动识别
         /* 名词: 阳性, 阴性, 中性, (默认)自动, 无
 		 * 数词: 基数, 序数, (默认)自动, 无
 		 * 动词: 第一变位, 第二变位, 特殊变位, 无
 		 */
-		// 重音所在的字母序号 或者 重音标 (*)
+        // 重音所在的字母序号 或者 重音标 (*)
 
         public string Translation { get; set; } // 词翻译
 		public string Word { get; set; } // 词本体
 		public string? WordType { get; set; } // 词类 if null give no tag
-		public string? SV { get; set; } // 对应完成体
-        public string? Direction { get; set; } // for motivate verbs that have directions.
+		public string? SV { get; set; } // 对应完成体 
+        public string? Direction { get; set; } // for motivate verbs that have directions. if not null give sign
 		public string? Follow { get; set; } // 接格关系
-
+		public int? HardIndex { get; set; } // 重音index
+		public bool ForceBasic { get; set; } = false; // force this word to be word=translate
 
 
 		public Slowo()
